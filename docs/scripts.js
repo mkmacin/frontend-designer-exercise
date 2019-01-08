0ee5aab735e957e01b8e5073bf8d9b65dabf4066
Mary Macin-Ditchen <mkmacin@ilstu.edu>
2019-01-08 14:06:18 -0600

    Formatted dates

--- docs/scripts.js
+++ docs/scripts.js
@@ -49,7 +49,12 @@ var app = new Vue({
             .catch(error => console.log(error))
     },
     methods: {
-
+        formattedDate: function(date) {
+            return moment(date).format('MMM Do YYYY [at] h:mm:ss a');
+        },
+        formattedBalance: function(balance) {
+            return numeral(balance).format('$0,0.00');
+        }
     },
     filters: {
 
